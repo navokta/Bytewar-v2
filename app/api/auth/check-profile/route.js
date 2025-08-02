88916f0ae6fbe376ea6a83789255896fde2d5ba8
bhavy-sharma <bhavymay18@gmail.com>
2025-08-03 00:55:47 +0530

    FIx Route In Complete Profile

--- app/api/auth/check-profile/route.js
+++ app/api/auth/check-profile/route.js
@@ -1,5 +1,5 @@
 import { getServerSession } from "next-auth/next";
-import { authOptions } from "../auth/[...nextauth]";
+import { authOptions } from "../[...nextauth]/route";
 import dbConnect from "@/lib/dbconnect";
 import User from "@/lib/models/user";
 
